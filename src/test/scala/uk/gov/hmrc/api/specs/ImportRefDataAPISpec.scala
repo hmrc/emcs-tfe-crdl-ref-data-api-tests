c518af9f65054073cbd8c181231cbec33189f09c
Kavitakum <79657475+Kavitakum@users.noreply.github.com>
2025-07-31 14:19:57 +0100

    CRDL-382 Added all the required acceptance tests for Ref data

--- src/test/scala/uk/gov/hmrc/api/specs/ImportRefDataAPISpec.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/ImportRefDataAPISpec.scala
@@ -37,6 +37,9 @@ class ImportRefDataAPISpec extends BaseSpec, HttpClient, BeforeAndAfterAll:
       correspondenceListResponse shouldBe Json.obj("status" -> "IDLE")
     }
 
+    deleteRefData("codelists")
+    deleteRefData("excise-products")
+    deleteRefData("cn-codes")
     importRefData().status shouldBe 202
     eventually {
       // Wait for the import job to finish
@@ -45,7 +48,7 @@ class ImportRefDataAPISpec extends BaseSpec, HttpClient, BeforeAndAfterAll:
     }
   }
 
-  Feature("User can test the import packaging types Ref data is done correctly") {
+  Feature("User can test the import of Ref data is done correctly") {
     Scenario("To verify whether Get Packaging Types Ref data request executes successfully") {
       Given("The endpoint is accessed")
       val authToken       = fetchAuthToken(): String
@@ -198,5 +201,134 @@ class ImportRefDataAPISpec extends BaseSpec, HttpClient, BeforeAndAfterAll:
       refDataObject("5").as[String] shouldBe "The product has been fortified for distillation"
       refDataObject("10")
         .as[String]                 shouldBe "The product has been made on the basis of experimental use of a new oenological practice"
+    }
+
+    Scenario("To verify whether Post Packaging Types Ref data request executes successfully") {
+      Given("The endpoint is accessed")
+      val authToken       = fetchAuthToken(): String
+      val refDataResponse = postRefData("oracle/packaging-types", """["IN","TC","CY","TK"]""", authToken)
+      refDataResponse.status shouldBe 200
+      val refDataObject = refDataResponse.body[JsValue].as[JsObject]
+      refDataObject("IN").as[String] shouldBe "Ingot"
+      refDataObject("TC").as[String] shouldBe "Tea-chest"
+      refDataObject("CY")
+        .as[String]                  shouldBe "Cylinder"
+      refDataObject("TK").as[String] shouldBe "Tank, rectangular"
+    }
+
+    Scenario("To verify whether Get EPC Codes Ref data request executes successfully") {
+      Given("The endpoint is accessed")
+      val authToken       = fetchAuthToken(): String
+      val refDataResponse = fetchRefData(authToken, "oracle/epc-codes ")
+      refDataResponse.status                                  shouldBe 200
+      refDataResponse.body[JsValue].as[List[JsValue]].take(3) shouldBe List(
+        Json.parse("""{
+             "code": "B000",
+            |    "description": "Beer",
+            |    "category": "B",
+            |    "categoryDescription": "Beer",
+            |    "unitOfMeasureCode": 3
+            |  }
+            |""".stripMargin),
+        Json.parse(""" {
+          "code": "E200",
+            |    "description": "Vegetable and animal oils Products falling within CN codes 1507 to 1518, if these are intended for use as heating fuel or motor fuel (Article 20(1)(a))",
+            |    "category": "E",
+            |    "categoryDescription": "Energy Products",
+            |    "unitOfMeasureCode": 2
+            |  }
+            |""".stripMargin),
+        Json.parse(""" {
+            "code": "E300",
+            |    "description": "Mineral oils Products falling within CN codes 2707 10, 2707 20, 2707 30 and 2707 50 (Article 20(1)(b))",
+            |    "category": "E",
+            |    "categoryDescription": "Energy Products",
+            |    "unitOfMeasureCode": 2
+            |  }
+            |""".stripMargin)
+      )
+    }
+
+    Scenario("To verify whether Retrieve CN Code Information for specific CN Codes request executes successfully") {
+      Given("The endpoint is accessed")
+      val authToken       = fetchAuthToken(): String
+      val refDataResponse = fetchRefData(authToken, "oracle/cn-codes/B000")
+      refDataResponse.status                                  shouldBe 200
+      refDataResponse.body[JsValue].as[List[JsValue]].take(3) shouldBe List(
+        Json.parse("""{
+             "cnCode": "22030001",
+            |    "cnCodeDescription": "Beer made from malt in bottles holding 10 litres or less",
+            |    "exciseProductCode": "B000",
+            |    "exciseProductCodeDescription": "Beer",
+            |    "unitOfMeasureCode": 3
+            |  }
+            |""".stripMargin),
+        Json.parse(""" {
+          "cnCode": "22030009",
+            |    "cnCodeDescription": "Beer made from malt in containers other than bottles holding 10 litres or less",
+            |    "exciseProductCode": "B000",
+            |    "exciseProductCodeDescription": "Beer",
+            |    "unitOfMeasureCode": 3
+            |  }
+            |""".stripMargin),
+        Json.parse(""" {
+            "cnCode": "22030010",
+            |    "cnCodeDescription": "Beer made from malt in containers holding more than 10 litres",
+            |    "exciseProductCode": "B000",
+            |    "exciseProductCodeDescription": "Beer",
+            |    "unitOfMeasureCode": 3
+            |  }
+            |""".stripMargin)
+      )
+    }
+
+    Scenario("To verify whether Post CN Code Information Ref data request executes successfully") {
+      Given("The endpoint is accessed")
+      val authToken       = fetchAuthToken(): String
+      val refDataResponse = postRefData(
+        "oracle/cn-code-information",
+        """{
+                                                                    |  "items": [{
+                                                                    |    "cnCode": "15081010",
+                                                                    |    "productCode": "E200"
+                                                                    |  }, {
+                                                                    |    "cnCode": "15089010",
+                                                                    |    "productCode": "E200"
+                                                                    |  }
+                                                                    ]
+                                                                    |}""".stripMargin,
+        authToken
+      )
+      refDataResponse.status shouldBe 200
+      val refDataObject = refDataResponse.body[JsValue].as[JsObject]
+      refDataObject("15081010") shouldBe Json.parse("""{
+                                                                       |    "cnCode": "15081010",
+                                                                       |    "cnCodeDescription": "Groundnut oil, crude, for technical or industrial uses other than the manufacture of foodstuffs for human consumption",
+                                                                       |    "exciseProductCode": "E200",
+                                                                       |    "exciseProductCodeDescription": "Vegetable and animal oils Products falling within CN codes 1507 to 1518, if these are intended for use as heating fuel or motor fuel (Article 20(1)(a))",
+                                                                       |    "unitOfMeasureCode": 2
+                                                                       |  }""".stripMargin)
+      refDataObject("15089010") shouldBe Json.parse("""{
+                                                                       |    "cnCode": "15089010",
+                                                                       |    "cnCodeDescription": "Groundnut oil, whether or not refined, not crude, for technical or industrial uses other than the manufacture of foodstuffs for human consumption",
+                                                                       |    "exciseProductCode": "E200",
+                                                                       |    "exciseProductCodeDescription": "Vegetable and animal oils Products falling within CN codes 1507 to 1518, if these are intended for use as heating fuel or motor fuel (Article 20(1)(a))",
+                                                                       |    "unitOfMeasureCode": 2
+                                                                       |  }""".stripMargin)
+    }
+
+    Scenario("To verify Delete Excise-Products request is successful") {
+      Given("The endpoint is accessed")
+      val deleteExciseProducts_response = deleteRefData("excise-products")
+      deleteExciseProducts_response.status shouldBe 200
+      val deleteCnCodes_response = deleteRefData("cn-codes")
+      deleteCnCodes_response.status shouldBe 200
+      val deleteCodelists_response = deleteRefData("codelists")
+      deleteCodelists_response.status shouldBe 200
+
+//      val authToken       = fetchAuthToken(): String
+//      val refDataResponse = fetchRefData(authToken, "oracle/wine-operations")
+//      refDataResponse.status        shouldBe 200
+//      refDataResponse.body[JsValue] shouldBe Json.arr()
     }
   }
